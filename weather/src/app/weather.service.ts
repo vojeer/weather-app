24eb9f437f546e44f9dd4d9829806bc2e6559719
vojeer <vojeerali@gmail.com>
2022-10-08 18:42:19 +0530

    <create service> for http call to get data from api

---
+++ weather/src/app/weather.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor() { }
+
+
+
+}
